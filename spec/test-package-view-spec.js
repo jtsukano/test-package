91eebed4432444eeafb57aecd0d8af4d3ea3ef7a
jtsukano <jaytsukano@gmail.com>
2016-12-01 16:59:28 -0800

    first commit

---
+++ spec/test-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TestPackageView from '../lib/test-package-view';
+
+describe('TestPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
